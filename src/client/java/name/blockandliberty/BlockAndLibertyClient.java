1398539dc77cb2a9288a039acdc03356bf771eae
FliP0x <75698141+FliP0x@users.noreply.github.com>
2024-11-22 12:36:15 +0100

    Add files via upload

---
+++ src/client/java/name/blockandliberty/BlockAndLibertyClient.java
@@ -0,0 +1,10 @@
+package name.blockandliberty;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockAndLibertyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
